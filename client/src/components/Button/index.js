57716a891bb084fb46684451955d8b7b81a16438
Dung Nguyen <dudu.nt107@gmail.com>
2020-11-30 22:34:25 -0600

    updated Button component

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./style.css";
 
-function DeleteBtn(props) {
+function Button(props) {
   return (
     <span className="btn" {...props} role="button" tabIndex="0">{props.children}
     </span>
   );
 }
 
-export default DeleteBtn;
+export default Button;
