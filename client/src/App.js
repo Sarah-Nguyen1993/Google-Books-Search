c9acddf803d966b32d7415bef521d7e1fc23d5c8
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-01 17:47:45 -0600

    created routes for different pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <div>
+        <Navbar/>
+        <Switch>
+          <Route exact path="/saved" component={Saved}/>
+          <Route exact path={["/","/search"]} component={Search}/>
+          <Route  component={NoMatch}/>
+        </Switch>
+      </div>
+      </Router>
     </div>
   );
 }
