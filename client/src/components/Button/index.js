e333a114603f0c02b552a8289abf1925b0043111
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-01 19:53:08 -0600

    updated Btn component

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -3,8 +3,8 @@ import "./style.css";
 
 function Button(props) {
   return (
-    <span className="btn" {...props} role="button" tabIndex="0">{props.children}
-    </span>
+  <button className={`btn btn-${props.type}`} {...props}>{props.children}
+    </button>
   );
 }
 
