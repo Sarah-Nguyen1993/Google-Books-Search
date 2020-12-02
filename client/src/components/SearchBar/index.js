4e04cf4c9f8ca3375d49172bf149f137694923d1
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-01 19:54:53 -0600

    updated search bar component

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Button from "../Button";
 
 function SearchBar(props) {
 
   return (
-        <div className="form-group">
+          <div className="form-group">
             <h5>Book Search</h5>
             <input className="form-control" {...props} />
-            <Button {...props} >Search </Button>
-        </div>
+          </div>  
   );
 }
 
