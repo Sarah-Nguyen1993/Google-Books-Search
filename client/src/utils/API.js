89d7b1c4126b8c536af8fdfb2931c5c952fe88bd
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-03 23:37:31 -0600

    added save,get,delete book routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,24 @@ import axios from "axios";
 const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
 
 export default {
-  getBooks: function(query) {
+  //search books from google api
+  searchBooks: function(query) {
     return axios.get(BASEURL + query)
+  },
+
+  //save books to the database
+  saveBook: function(bookData){
+    return axios.post("/api/books", bookData)
+  },
+
+  //get all books from the database
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+
+  //delete book with given id
+  deleteBook: function(id){
+    return axios.delete("/api/books/" + id)
   }
 };
+  
