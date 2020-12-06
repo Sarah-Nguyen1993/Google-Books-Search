b5e81749f22c8ae32b2123e3ef6ba9cc0f353a45
Dung Nguyen <dudu.nt107@gmail.com>
2020-12-05 20:15:15 -0600

    changed name of some elements

--- models/book.js
+++ models/book.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
     title: {type:String,required: true},
-    authors:{ type:Array, required:true},
-    description:{ type:String, required:true},
-    image:{ type:String, required:true},
-    link:{ type:String, required:true},
+    authors:{ type:Array},
+    description:{ type:String},
+    thumbnail:{ type:String},
+    bookLink:{ type:String, required:true},
     date: { type: Date, default: Date.now }
 });
 
